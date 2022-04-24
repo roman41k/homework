39a1499df1820aea56a622c0599f3b97c3b4f4df
trockenroman@gmail.com <trockenroman@gmail.com>
2022-04-24 09:36:54 +0300

    Update lesson-1.js

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -1,7 +1,4 @@
-const name = 1;
-console.log(name);
-console.log(1+2);
-console.log(4+5);
-const nameSecond = 'vasya';
-console.log(nameSecond);
-console.log(name + nameSecond);
+let quantity = prompt('Введите колличество товара');
+quantity = Number(quantity);
+console.log(quantity);
+console.log(typeof quantity);
