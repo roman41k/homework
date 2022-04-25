e0987ce7569459de16183eed77252ec721760ab5
trockenroman@gmail.com <trockenroman@gmail.com>
2022-04-25 14:17:31 +0300

    JavaScript lernen.

---
+++ js/cycle_do_while copy.js
@@ -0,0 +1,6 @@
+let clientCounter = 18;
+const maxClients = 25;
+while (clientCounter < maxClients) { 
+    console.log(clientCounter);
+    clientCounter += 1;
+}
--- js/lesson-1.js
+++
@@ -1,2 +0,0 @@
-let salaray = 1300.4548485;
-console.log(Number(salaray.toFixed(2)));
